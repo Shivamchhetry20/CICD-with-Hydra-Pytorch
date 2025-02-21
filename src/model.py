6648d4bf890608b3237875fc6af8e432225fa793
Shivamchhetry20 <shivamchhetry20@gmail.com>
2025-02-21 21:24:52 +0530

    adding the final scripts

--- src/model.py
+++ src/model.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
 
+#NOTE: this is the simple NN that I used for testing purpose
 class SimpleNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(SimpleNN, self).__init__()
@@ -10,4 +12,22 @@ class SimpleNN(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
+        return x
+
+#NOTE: This the CNN model that is the base model
+class CNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=input_size, out_channels=hidden_size, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels=hidden_size, out_channels=16, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.fc1 = nn.Linear(16*7*7, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
         return x
